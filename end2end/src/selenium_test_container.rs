05c59715d6aa67504705316e22287a36182579c6
gusti <gusti@gustis-MacBook-Pro.local>
2025-03-14 20:05:18 +0700

    add host

--- end2end/src/selenium_test_container.rs
+++ end2end/src/selenium_test_container.rs
@@ -59,7 +59,7 @@ async fn selenium_node() -> &'static Mutex<Option<ContainerAsync<Selenium>>> {
             let container = Selenium::default()
                 .with_cmd([
                     "/opt/bin/entry_point.sh",
-                    "--net=host",
+                    "--add-host=host.docker.internal:host-gateway",
                     r#"--shm-size="2g""#,
                 ])
                 .start()
